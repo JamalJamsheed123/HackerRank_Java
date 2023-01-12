d4995ab9881e89cc025795043d29fb1fc631a4bd
JamalJamsheed123 <jamal.jamsheed@gmail.com>
2023-01-12 16:14:00 +0500

    update the changes \\Welcome to Java \\stdin and stdout \\java If-Else \\Java Output Formatting \\Java Loop

---
+++ JAVA/JavaIfElse.java
@@ -0,0 +1,48 @@
+// Task 
+// Given an integer, , perform the following conditional actions:
+// If  is odd, print Weird
+// If  is even and in the inclusive range of  to , print Not Weird
+// If  is even and in the inclusive range of  to , print Weird
+// If  is even and greater than , print Not Weird
+// Complete the stub code provided in your editor to print whether or not  is weird.
+// Input Format
+// A single line containing a positive integer, .
+// Constraints
+
+// Output Format
+// Print Weird if the number is weird; otherwise, print Not Weird.
+// Sample Input 0
+// 3
+// Sample Output 0
+// Weird
+// Sample Input 1
+// 24
+// Sample Output 1
+// Not Weird
+
+package JAVA;
+import java.util.*;
+
+public class JavaIfElse {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+         if (N % 2 == 0 && N >= 2 && N <= 5 ) {
+            System.out.println("Not Weird");
+
+        } else if (N % 2 == 0 && N > 20) {
+            System.out.println("Not Weird");
+        } else {
+            System.out.println("Weird");
+        }
+        
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        scanner.close();
+    }
+}
+
