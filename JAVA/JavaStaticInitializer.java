61f17ce6f1eb8e9127eea3256309728eed50ca08
JamalJamsheed123 <jamal.jamsheed@gmail.com>
2023-01-24 18:22:42 +0500

    Add Java static Initializer block file in Hacker-rank java Prepare

---
+++ JAVA/JavaStaticInitializer.java
@@ -0,0 +1,35 @@
+package JAVA;
+
+import java.util.*;
+
+
+public class JavaStaticInitializer {
+
+    private static boolean flag;
+    private static int B;
+    private static int H;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        if (B > 0 && H > 0) {
+            flag = true;
+        }
+        else {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        sc.close();
+    }
+
+    public static void main(String[] args){
+            if(flag){
+                int area=B*H;
+                System.out.print(area);
+            }
+            
+        }
+
+}
+
